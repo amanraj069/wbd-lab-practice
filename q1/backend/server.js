512bdbfae15eda5cc4096c1e133a9709b1584470
jahnavi <jahnavipatnam@gmail.com>
2026-05-03 18:02:11 +0530

    events crud done

--- q1/backend/server.js
+++ q1/backend/server.js
@@ -8,6 +8,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+function auth(req, res, next){
+    const token = req.headers.authorization;
+
+    if(!token) return res.status(401).send("No token");
+
+    try{
+        const decoded = require("jsonwebtoken").verify(token, "secret");
+        req.user = decoded;
+        next();
+    } catch {
+        res.status(401).send("Invalid token");
+    }
+}
+
 app.get("/", (req, res) => {
     res.send("Server running");
 });
@@ -44,5 +58,60 @@ app.post("/login", (req, res) => {
 
     res.json({token});
 });
+
+app.post("/events", auth, (req, res) => {
+    const events = JSON.parse(fs.readFileSync("./data/events.json"));
+
+    const newEvent = {
+        id: Date.now(),
+        title: req.body.title,
+        date: req.body.date,
+        time: req.body.time,
+        location: req.body.location,
+        createdBy: req.user.id
+    };
+
+    events.push(newEvent);
+
+    fs.writeFileSync("./data/events.json", JSON.stringify(events));
+
+    res.send("Event created");
+});
+
+app.get("/events", (req, res) => {
+    const events = JSON.parse(fs.readFileSync("./data/events.json"));
+    res.json(events);
+});
+
+app.get("/event/:id", (req, res) => {
+    const events = JSON.parse(fs.readFileSync("./data/events.json"));
+    const event = events.find(e => e.id === req.params.id);
+
+    if(!event) return res.status(404).send("Not found");
+    
+    res.json(event);
+});
+
+app.put("/events/:id", auth, (req, res) => {
+    let events = JSON.parse(fs.readFileSync("./data/events.json"));
+
+    events = events.map(e => 
+        e.id == req.params.id ? {...e, ...req.body} : e
+    );
+
+    fs.writeFileSync("./data/events.json", JSON.stringify(events));
+
+    res.send("Updated");
+});
+
+app.delete("/events/:id", auth, (req, res) => {
+    let events = JSON.parse(fs.readFileSync("./data/events.json"));
+
+    events = events.filter(e => e.id != req.params.id);
+
+    fs.writeFileSync("./data/events.json", JSON.stringify(events));
+
+    res.send("Deleted");
+})
 
 app.listen(5000, () => console.log("Server running on port 5000"));
